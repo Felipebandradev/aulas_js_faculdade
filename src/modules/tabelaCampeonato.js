7d7bd5f190d73faff1387d5d2e488f89c35eb6aa
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2025-03-22 16:27:38 -0300

    feat: Exercicios 5 e css
    adicionado o exercicio05 e css na tabela

---
+++ src/modules/tabelaCampeonato.js
@@ -0,0 +1,60 @@
+console.log("Olá mundo!");
+
+// Capturando a tabela pelo id
+const Tabela_campeonato = document.getElementById("tabelaCampeonato");
+
+// Array associativo para injeção de dados na tabela
+const ListaTimes = [
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Botafogo_de_Futebol_e_Regatas_logo.svg/1816px-Botafogo_de_Futebol_e_Regatas_logo.svg.png",
+    posicao: 1,
+    siteTime: "https://www.espn.com.br/futebol/time/_/id/6086/botafogo",
+    nome: "Bota Fogo",
+    pontos: 20,
+    qtdJogos: 10,
+    qtdVitoria: 10,
+    qtdDerrotas: 0,
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Palmeiras_logo.svg/2048px-Palmeiras_logo.svg.png",
+    posicao: 2,
+    siteTime: "https://www.espn.com.br/futebol/time/_/id/2029/palmeiras",
+    nome: "Palmeiras",
+    pontos: 19,
+    qtdJogos: 10,
+    qtdVitoria: 9,
+    qtdDerrotas: 1,
+  },
+
+  {
+    img: "https://seeklogo.com/images/F/flamengo-de-grossos-logo-DF503973A8-seeklogo.com.png",
+    posicao: 3,
+    siteTime: "https://www.espn.com.br/futebol/time/_/id/819/flamengo",
+    nome: "Flamengo",
+    pontos: 10,
+    qtdJogos: 9,
+    qtdVitoria: 8,
+    qtdDerrotas: 1,
+  },
+];
+
+// Injetando dados na tabela
+ListaTimes.map((time) => {
+  Tabela_campeonato.innerHTML += `  
+        <tr id="${time.nome}">
+            <td><img src=${time.img} alt="Logo ${time.nome}" /></td>
+            <td>${time.posicao}</td>
+            <td><p><a href=${time.siteTime} target="_blank">${time.nome}</a></p></td>
+            <td>${time.pontos}</td>
+            <td>${time.qtdJogos}</td>
+            <td>${time.qtdVitoria}</td>
+            <td>${time.qtdDerrotas}</td>
+        </tr> 
+    `;
+
+  if (time.posicao == 1) {
+    const vencedor = document.getElementById(`${time.nome}`);
+    vencedor.style.backgroundColor = "darkgreen";
+    vencedor.style.color = "white";
+  }
+});
